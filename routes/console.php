908e789fe199d245ed35fdc3057017d32c1f3ec9
vrayoga <vrayogal227@gmail.com>
2025-06-21 23:17:41 +0700

    penambahan jadwal otomatis

--- routes/console.php
+++ routes/console.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 // Jadwalkan command di sini
-//Schedule::command('check:overdue-peminjaman')->dailyAt('00:00');
+Schedule::command('check:overdue-peminjaman')->dailyAt('00:00');
 // Atau untuk testing:
-Schedule::command('check:overdue-peminjaman')->everyMinute();
+//Schedule::command('check:overdue-peminjaman')->everyMinute();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
